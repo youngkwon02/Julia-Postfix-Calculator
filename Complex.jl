6e85864c664ae8c1642a0acdee3fd8d3a7c6033c
youngkwon02 <youngkwon02@naver.com>
2021-11-19 02:28:14 +0900

    feat: Complex-Rational add and sub operation

--- Complex.jl
+++ Complex.jl
@@ -86,26 +86,30 @@ function AddOper(val1::Rational, val2::Complex)
   RealPart = FractionParse(RationalWithOneSlash(string(real(result))))
   ImagPart = FractionParse(RationalWithOneSlash(string(imag(result))))
   if imag(result) > 0
-    strResult = RealPart * "+" * ImagPart * "im"
+    strResult = RealPart * " + " * ImagPart * "i"
   elseif imag(result) < 0
-    strResult = RealPart * ImagPart * "im"
+    strResult = RealPart * " " * ImagPart * "i"
   else
     return [Rational, RealPart]
   end
   return [Complex, strResult]
 end
 
-# function AddOper(val1::Complex, val2::Rational)
-#   C_Value1 = ComplexParse(val1)
-#   C_Value2 = parse(Float64, val2.value)
-#   strResult = string(C_Value1 + C_Value2)
-#   if HavingImaginePart(strResult)
-#     result = ImToI(strResult)
-#     return [Complex, result]
-#   end
-#   strResult = string(real(parse(Base.Complex{Float64}, strResult)))
-#   return [Float, strResult]
-# end
+function AddOper(val1::Complex, val2::Rational)
+  C_Value1 = ComplexParse(val1)
+  C_Value2 = RationalParse(val2)
+  result = C_Value1 + C_Value2
+  RealPart = FractionParse(RationalWithOneSlash(string(real(result))))
+  ImagPart = FractionParse(RationalWithOneSlash(string(imag(result))))
+  if imag(result) > 0
+    strResult = RealPart * " + " * ImagPart * "i"
+  elseif imag(result) < 0
+    strResult = RealPart * " " * ImagPart * "i"
+  else
+    return [Rational, RealPart]
+  end
+  return [Complex, strResult]
+end
 
 function SubOper(val1::Complex, val2::Complex)
   C_Value1 = ComplexParse(val1)
@@ -117,4 +121,84 @@ function SubOper(val1::Complex, val2::Complex)
   end
   strResult = string(real(parse(Base.Complex{Float64}, strResult)))
   return [Float, strResult]
+end
+
+function SubOper(val1::Integer, val2::Complex)
+  C_Value1 = parse(Int64, val1.value)
+  C_Value2 = ComplexParse(val2)
+  strResult = string(C_Value2 - C_Value1)
+  if HavingImaginePart(strResult)
+    result = ImToI(strResult)
+    return [Complex, result]
+  end
+  strResult = string(real(parse(Base.Complex{Float64}, strResult)))
+  return [Float, strResult]
+end
+
+function SubOper(val1::Complex, val2::Integer)
+  C_Value1 = ComplexParse(val1)
+  C_Value2 = parse(Int64, val2.value)
+  strResult = string(C_Value2 - C_Value1)
+  if HavingImaginePart(strResult)
+    result = ImToI(strResult)
+    return [Complex, result]
+  end
+  strResult = string(real(parse(Base.Complex{Float64}, strResult)))
+  return [Float, strResult]
+end
+
+function SubOper(val1::Float, val2::Complex)
+  C_Value1 = parse(Float64, val1.value)
+  C_Value2 = ComplexParse(val2)
+  strResult = string(C_Value2 - C_Value1)
+  if HavingImaginePart(strResult)
+    result = ImToI(strResult)
+    return [Complex, result]
+  end
+  strResult = string(real(parse(Base.Complex{Float64}, strResult)))
+  return [Float, strResult]
+end
+
+function SubOper(val1::Complex, val2::Float)
+  C_Value1 = ComplexParse(val1)
+  C_Value2 = parse(Float64, val2.value)
+  strResult = string(C_Value2 - C_Value1)
+  if HavingImaginePart(strResult)
+    result = ImToI(strResult)
+    return [Complex, result]
+  end
+  strResult = string(real(parse(Base.Complex{Float64}, strResult)))
+  return [Float, strResult]
+end
+
+function SubOper(val1::Rational, val2::Complex)
+  C_Value1 = RationalParse(val1)
+  C_Value2 = ComplexParse(val2)
+  result = C_Value2 - C_Value1
+  RealPart = FractionParse(RationalWithOneSlash(string(real(result))))
+  ImagPart = FractionParse(RationalWithOneSlash(string(imag(result))))
+  if imag(result) > 0
+    strResult = RealPart * " + " * ImagPart * "i"
+  elseif imag(result) < 0
+    strResult = RealPart * " " * ImagPart * "i"
+  else
+    return [Rational, RealPart]
+  end
+  return [Complex, strResult]
+end
+
+function SubOper(val1::Complex, val2::Rational)
+  C_Value1 = ComplexParse(val1)
+  C_Value2 = RationalParse(val2)
+  result = C_Value2 - C_Value1
+  RealPart = FractionParse(RationalWithOneSlash(string(real(result))))
+  ImagPart = FractionParse(RationalWithOneSlash(string(imag(result))))
+  if imag(result) > 0
+    strResult = RealPart * " + " * ImagPart * "i"
+  elseif imag(result) < 0
+    strResult = RealPart * ImagPart * "i"
+  else
+    return [Rational, RealPart]
+  end
+  return [Complex, strResult]
 end
