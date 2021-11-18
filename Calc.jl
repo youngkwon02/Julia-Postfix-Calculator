94b5247d9fddfb37d257f468ed478c2c9168b334
youngkwon02 <youngkwon02@naver.com>
2021-11-18 15:20:15 +0900

    refactor: Calculator operation data type

--- Calc.jl
+++ Calc.jl
@@ -24,48 +24,54 @@ function add()
   val1 = pop!(STACK)
   val2 = pop!(STACK)
   typeAndResult = AddOper(val1, val2)
-  typeAndResult[2] = floor(typeAndResult[2], digits = 10)
-  push!(STACK, typeAndResult[1](string(typeAndResult[2])))
+  push!(STACK, typeAndResult[1](typeAndResult[2]))
   println(typeAndResult[2])
 end
 
 function AddOper(val1::Integer, val2::Integer)
-  return [Float, parse(Float64, val1.value) + parse(Float64, val2.value)]
+  result = floor(parse(Float64, val1.value) + parse(Float64, val2.value), digits = 10)
+  return [Float, string(result)]
 end
 
 function AddOper(val1::Integer, val2::Float)
-  return [Float, parse(Float64, val1.value) + parse(Float64, val2.value)]
+  result = floor(parse(Float64, val1.value) + parse(Float64, val2.value), digits = 10)
+  return [Float, string(result)]
 end
 
 function AddOper(val1::Float, val2::Integer)
-  return [Float, parse(Float64, val1.value) + parse(Float64, val2.value)]
+  result = floor(parse(Float64, val1.value) + parse(Float64, val2.value), digits = 10)
+  return [Float, string(result)]
 end
 
 function AddOper(val1::Float, val2::Float)
-  return [Float, parse(Float64, val1.value) + parse(Float64, val2.value)]
+  result = floor(parse(Float64, val1.value) + parse(Float64, val2.value), digits = 10)
+  return [Float, string(result)]
 end
 
 function sub()
   val1 = pop!(STACK)
   val2 = pop!(STACK)
   typeAndResult = SubOper(val1, val2)
-  typeAndResult[2] = floor(typeAndResult[2], digits = 10)
-  push!(STACK, typeAndResult[1](string(typeAndResult[2])))
+  push!(STACK, typeAndResult[1](typeAndResult[2]))
   println(typeAndResult[2])
 end
 
 function SubOper(val1::Integer, val2::Integer)
-  return [Float, parse(Float64, val2.value) - parse(Float64, val1.value)]
+  result = floor(parse(Float64, val2.value) - parse(Float64, val1.value), digits = 10)
+  return [Float, string(result)]
 end
 
 function SubOper(val1::Integer, val2::Float)
-  return [Float, parse(Float64, val2.value) - parse(Float64, val1.value)]
+  result = floor(parse(Float64, val2.value) - parse(Float64, val1.value), digits = 10)
+  return [Float, string(result)]
 end
 
 function SubOper(val1::Float, val2::Integer)
-  return [Float, parse(Float64, val2.value) - parse(Float64, val1.value)]
+  result = floor(parse(Float64, val2.value) - parse(Float64, val1.value), digits = 10)
+  return [Float, string(result)]
 end
 
 function SubOper(val1::Float, val2::Float)
-  return [Float, parse(Float64, val2.value) - parse(Float64, val1.value)]
+  result = floor(parse(Float64, val2.value) - parse(Float64, val1.value), digits = 10)
+  return [Float, string(result)]
 end
